03963ac4db14a0ad66bb33b3c7c2db5d96050796
Mehdi Benamar <mistahbe213@gmail.com>
2024-12-08 11:52:40 +0100

    Edit style for Button component

--- FRONT/src/components/Button.vue
+++ FRONT/src/components/Button.vue
@@ -5,31 +5,6 @@ withDefaults(defineProps<{ label: string, outline: boolean }>(), {outline: false
 </script>
 
 <template>
-  <Button :label="label" :class="outline ? $style.buttonOutlined : $style.buttonFilled"
+  <Button :label="label" :class="outline ? 'buttonOutlined' : 'buttonFilled'"
           :variant="outline ? 'outlined' : undefined"/>
 </template>
-
-<style scoped module>
-.buttonFilled {
-  background-color: var(--primary);
-  border: none;
-}
-
-.buttonOutlined {
-  color: var(--background);
-}
-
-.buttonFilled:not(:disabled):hover {
-  background-color: var(--accent);
-  border: none;
-}
-
-.buttonOutlined {
-  border-color: var(--primary);
-}
-
-.buttonOutlined:not(:disabled):hover {
-  border-color: var(--primary);
-  background-color: var(--background-90);
-}
-</style>
