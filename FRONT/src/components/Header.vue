45697f199e708372eac2d6fe4c4f5e52dfd7b989
Mehdi Benamar <mistahbe213@gmail.com>
2024-12-15 13:57:33 +0100

    add RouterLink component

--- FRONT/src/components/Header.vue
+++ FRONT/src/components/Header.vue
@@ -1,50 +1,54 @@
 <script setup lang="ts">
-
+import {RouterLink} from "vue-router";
 </script>
 
 <template>
   <header class="header">
-      <div class="container">
-        <p class="header-title">Sadaqa</p>
-        <nav class="header-nav">
-          <ul class="header-list">
-            <li class="header-item">Rechercher une cagnotte</li>
-            <li class="header-item">Connexion</li>
-            <li class="header-item"><a href="" class="primary-button">Créer une cagnotte</a></li>
-          </ul>
-        </nav>
-      </div>
+    <div class="container">
+      <p class="header-title">Sadaqa</p>
+      <nav class="header-nav">
+        <ul class="header-list">
+          <li class="header-item">Rechercher une cagnotte</li>
+          <li class="header-item">
+            <RouterLink :to="{name: 'login'}">Connexion</RouterLink>
+          </li>
+          <li class="header-item">
+            <RouterLink :to="{name: 'register'}" class="primary-button">S'inscrire</RouterLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
 <style scoped>
 
-header{
+header {
   display: flex;
   justify-content: center;
   height: 60px;
-  border-bottom: 1px solid rgba(0,0,0,0.1);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 }
 
-.header .container{
+.header .container {
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 8px;
 }
 
-.header-nav{
+.header-nav {
   width: 560px;
 }
 
-.header-list{
+.header-list {
   display: flex;
-  justify-content: space-around ;
+  justify-content: space-around;
   align-items: center;
   list-style: none;
 }
 
-.header-item:hover{
+.header-item:hover {
   color: var(--primary);
   cursor: pointer;
 }
