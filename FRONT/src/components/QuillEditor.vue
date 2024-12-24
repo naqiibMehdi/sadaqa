df699ae32786b41c11e429633d351688e627af86
Mehdi Benamar <mistahbe213@gmail.com>
2024-12-24 19:38:52 +0100

    edit Quill Editor

--- FRONT/src/components/QuillEditor.vue
+++ FRONT/src/components/QuillEditor.vue
@@ -1,41 +1,36 @@
 <script setup lang="ts">
-import Quill from "quill"
+import Editor from "primevue/editor"
 import "quill/dist/quill.snow.css"
-import {onMounted, ref} from "vue";
-
-let editor = ref<any | Quill>(null)
-let editorRef = ref("")
 
 
-onMounted(() => {
-  editor.value = new Quill(editorRef.value, {
-    theme: "snow",
-    modules: {
-      toolbar: [
-        ['bold', 'italic', 'underline', 'link', 'image'],
-      ]
-    }
-  })
-  editor.value.root.style.fontSize = "1rem"
-})
 </script>
 
 <template>
+  <!--  <div class="quill-group">-->
+  <!--    <label for="">Description</label>-->
+  <!--    <div class="quill-editor">-->
+  <!--      <div ref="editorRef" class="quill-container"></div>-->
+  <!--    </div>-->
+  <!--  </div>-->
   <div class="quill-group">
     <label for="">Description</label>
-    <div class="quill-editor">
-      <div ref="editorRef" class="quill-container"></div>
-    </div>
+    <Editor editorStyle="height: 180px; font-size: 1rem" class="quill-editor">
+      <template v-slot:toolbar>
+            <span class="ql-formats">
+                <button v-tooltip.bottom="'Bold'" class="ql-bold"></button>
+                <button v-tooltip.bottom="'Italic'" class="ql-italic"></button>
+                <button v-tooltip.bottom="'Underline'" class="ql-underline"></button>
+                <button v-tooltip.bottom="'link'" class="ql-link"></button>
+                <button v-tooltip.bottom="'image'" class="ql-image"></button>
+            </span>
+      </template>
+    </Editor>
   </div>
 </template>
 
 <style scoped>
+
 .quill-editor {
   margin-top: .9rem;
-}
-
-.quill-container {
-  min-height: 150px;
-  background-color: #fff;
 }
 </style>
