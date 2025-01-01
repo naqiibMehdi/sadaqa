d8cf2381baa8453166bc238984774aa42ae63013
mehdi <mistahbe213@gmail.com>
2025-01-01 18:10:06 +0100

    url api group by Auth and test

--- BACK/routes/api.php
+++ BACK/routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
+use \App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix("auth")->group(function () {
+    Route::post("/register", [UserController::class, "index"]);
+});
+
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
