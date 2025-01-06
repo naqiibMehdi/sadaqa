a19a3c5a820d87f801d1d18acea45850dd1d9bcf
Mehdi Benamar <mistahbe213@gmail.com>
2025-01-06 17:36:53 +0100

    Create databes sessions

--- BACK/database/migrations/0001_01_01_000000_create_users_table.php
+++ BACK/database/migrations/0001_01_01_000000_create_users_table.php
@@ -8,8 +8,8 @@ return new class extends Migration {
     /**
      * Run the migrations.
      */
-//    public function up(): void
-//    {
+   public function up(): void
+    {
 //        Schema::create('users', function (Blueprint $table) {
 //            $table->id();
 //            $table->string('name');
@@ -26,24 +26,24 @@ return new class extends Migration {
 //            $table->timestamp('created_at')->nullable();
 //        });
 //
-//        Schema::create('sessions', function (Blueprint $table) {
-//            $table->string('id')->primary();
-//            $table->foreignId('user_id')->nullable()->index();
-//            $table->string('ip_address', 45)->nullable();
-//            $table->text('user_agent')->nullable();
-//            $table->longText('payload');
-//            $table->integer('last_activity')->index();
-//        });
-//    }
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+           $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+           $table->integer('last_activity')->index();
+        });
+    }
 
     /**
      * Reverse the migrations.
      */
-//    public function down(): void
-//    {
-//        Schema::dropIfExists('users');
-//        Schema::dropIfExists('password_reset_tokens');
-//        Schema::dropIfExists('sessions');
-//    }
+    public function down(): void
+   {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
 };
 
