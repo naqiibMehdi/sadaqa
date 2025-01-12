a8d20ed328c49ea1ed130d074850068a90604611
mehdi <mistahbe213@gmail.com>
2025-01-12 20:26:35 +0100

    add management of cover image for campaigns on each routes

--- BACK/app/Http/Controllers/Api/CampaignController.php
+++ BACK/app/Http/Controllers/Api/CampaignController.php
@@ -5,37 +5,50 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCampaignRequest;
 use App\Models\Campaign;
-use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class CampaignController extends Controller
 {
     public function index(): JsonResponse
     {
-        return response()->json(["campaigns" => Campaign::paginate(9)]);
+        $campaigns = Campaign::paginate(9);
+
+        $campaigns->getCollection()->transform(function ($campaign) {
+            $campaign->url_image = asset("storage/" . $campaign->image);
+            return $campaign;
+        });
+
+        return response()->json(["campaigns" => $campaigns]);
     }
 
 
     public function show(string $slug, string $id): JsonResponse
     {
         $campaign = Campaign::where("id", $id)->where('slug', $slug)->first();
+        if ($campaign) {
+            $campaign->url_image = asset("storage/" . $campaign->image);
+        }
         return response()->json(["campaign" => !$campaign ? [] : $campaign]);
     }
 
     public function store(StoreCampaignRequest $request): JsonResponse
     {
+        $validated = $request->validated();
+        $slug = Str::slug($validated["title"]);
+        $imagePath = "default_cover_campaign.png";
+
+        if ($request->hasFile("image") && $request->file("image")->isValid()) {
+            $imagePath = $request->file("image")->store("campaigns", "public");
+        }
+
         Campaign::create([
-            "title" => $request->input("title"),
-            "description" => $request->input("description"),
-            "image" => "default.png",
-            "slug" => Str::slug($request->input("title")),
-            "target_amount" => $request->input("target_amount"),
-            "collected_amount" => 0,
-            "limit_date" => $request->input("limit_date"),
-            "category_id" => $request->input("category_id"),
-            "user_id" => auth()->id()
+            ...$validated,
+            "slug" => $slug,
+            "image" => $imagePath,
+            "user_id" => Auth::id(),
         ]);
 
         return response()->json(["message" => "votre cagnotte a été créée avec succès !"]);
@@ -44,7 +57,7 @@ class CampaignController extends Controller
     public function update(string $slug, string $id, StoreCampaignRequest $request): JsonResponse
     {
         $campaign = Campaign::where("id", $id)->where('slug', $slug)->firstOrFail();
-
+        $validated = $request->validated();
 
         if ($campaign->user_id !== Auth::id()) {
             return response()->json(["message" => "vous n'êtes pas autorisé à modifier cet cagnotte"], 403);
@@ -54,9 +67,21 @@ class CampaignController extends Controller
             $campaign->slug = Str::slug($request->validated("title"));
         }
 
-        $campaign->update($request->validated());
+        if ($request->hasFile("image") && $request->file("image")->isValid()) {
+            $imagePath = $request->file("image")->store("campaigns", "public");
 
-        return response()->json(["message" => "Cagnotte mis à jour avec succès", "campaign" => $campaign]);
+            if ($campaign->image === "default_cover_campaign.png") {
+                $validated["image"] = $imagePath;
+            } else {
+                Storage::disk("public")->delete($campaign->image);
+                $validated["image"] = $imagePath;
+            }
+        }
+
+        $campaign->update($validated);
+
+
+        return response()->json(["message" => "Cagnotte mis à jour avec succès"]);
     }
 
     public function destroy(string $slug, string $id): JsonResponse
@@ -66,6 +91,10 @@ class CampaignController extends Controller
 
         if ($campaign->user_id !== Auth::id()) {
             return response()->json(["message" => "vous n'êtes pas autorisé à supprimer cet cagnotte"], 403);
+        }
+
+        if ($campaign->image !== "default_cover_campaign.png") {
+            Storage::disk("public")->delete($campaign->image);
         }
 
         $campaign->delete();
