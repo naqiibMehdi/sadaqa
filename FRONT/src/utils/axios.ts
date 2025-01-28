1a1ad086b757395924dd8f48922b1becad4d1b87
mehdi <mistahbe213@gmail.com>
2025-01-28 17:27:50 +0100

    add interceptor

--- FRONT/src/utils/axios.ts
+++ FRONT/src/utils/axios.ts
@@ -1,4 +1,6 @@
-import axios from 'axios'
+import axios, {AxiosError} from 'axios'
+import {useAuthStore} from "@/stores/useAuthStore.ts";
+
 
 const api = axios.create({
     baseURL: 'http://localhost:8000/api',
@@ -8,4 +10,37 @@ const api = axios.create({
     }
 })
 
-export default api
+export const fetchData = async (endpoint: string) => {
+    try {
+        const response = await api.get(endpoint)
+        return response.data
+    } catch (error) {
+        if (error instanceof AxiosError) {
+            throw error
+        }
+    }
+}
+export const postData = async (endpoint: string, data: object) => {
+    try {
+        const response = await api.post(endpoint, data)
+        return response.data
+    } catch (error) {
+        if (error instanceof AxiosError) {
+            throw error
+        }
+    }
+}
+
+api.interceptors.request.use(
+    (config) => {
+        const authStore = useAuthStore();
+        const token = authStore.token
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
