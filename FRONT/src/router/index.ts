f53157f7d1ff9db11a3462f3aef052a5966bed55
mehdi <mistahbe213@gmail.com>
2025-01-28 17:33:03 +0100

    add meta key in routes to protect routes

--- FRONT/src/router/index.ts
+++ FRONT/src/router/index.ts
@@ -9,24 +9,38 @@ import CampaignsView from "@/pages/CampaignsView.vue";
 import CampaignView from "@/pages/CampaignView.vue"
 import DashBoardView from "@/pages/DashBoardView.vue";
 import FormForgetPassword from "@/pages/FormForgetPassword.vue";
+import {useAuthStore} from "@/stores/useAuthStore.ts";
 
 
 const routes = [
-    {path: "/login", name: "login", component: FormLoginView},
+    {path: "/login", name: "login", component: FormLoginView, meta: {requireAuth: false}},
+    {path: "/register", name: "register", component: FormRegisterView, meta: {requireAuth: false}},
     {path: "/forget-password", name: "forget-password", component: FormForgetPassword},
-    {path: "/register", name: "register", component: FormRegisterView},
     {path: "/contact", name: "contact", component: FormContact},
-    {path: "/create-campaign", name: "createcampaign", component: FormCreateCampaignView},
-    {path: "/profil", name: "profil", component: ProfilView},
-    {path: "/payment", name: "payment", component: FormPaymentView},
+    {path: "/create-campaign", name: "createcampaign", component: FormCreateCampaignView, meta: {requireAuth: true}},
+    {path: "/profil", name: "profil", component: ProfilView, meta: {requireAuth: true}},
     {path: "/campaigns", name: "campaigns", component: CampaignsView},
     {path: "/campaign/titre-campaign", name: "campaign", component: CampaignView},
-    {path: "/dashboard", name: "dashboard", component: DashBoardView},
+    {path: "/dashboard", name: "dashboard", component: DashBoardView, meta: {requireAuth: true}},
+    {path: "/payment", name: "payment", component: FormPaymentView},
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, _, next) => {
+    const authStore = useAuthStore()
+    const hasToken = authStore.token
+
+    if (!to.meta.requireAuth && hasToken) {
+        next({name: "dashboard"})
+    } else if (to.meta.requireAuth && !hasToken) {
+        next({name: "login"})
+    } else {
+        next()
+    }
 })
 
 export default router
