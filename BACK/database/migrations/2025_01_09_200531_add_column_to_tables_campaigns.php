dc2a2f515e54a1888435aecb2c07f24fd787abe5
mehdi <mistahbe213@gmail.com>
2025-02-17 23:25:29 +0100

    edit type of closing-date field

--- BACK/database/migrations/2025_01_09_200531_add_column_to_tables_campaigns.php
+++ BACK/database/migrations/2025_01_09_200531_add_column_to_tables_campaigns.php
@@ -5,25 +5,25 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('campaigns', function (Blueprint $table) {
-            $table->string("slug")->after("image");
-            $table->date("closing_date")->nullable()->after("limit_date");
-        });
-    }
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::table('campaigns', function (Blueprint $table) {
+      $table->string("slug")->after("image");
+      $table->dateTime("closing_date")->nullable()->after("limit_date");
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('campaigns', function (Blueprint $table) {
-            $table->dropColumn("slug");
-            $table->dropColumn("closing_date");
-        });
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::table('campaigns', function (Blueprint $table) {
+      $table->dropColumn("slug");
+      $table->dropColumn("closing_date");
+    });
+  }
 };
