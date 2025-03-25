1987502b0281ebd7a879aba3828207d6727e2c4a
mehdi <mistahbe213@gmail.com>
2025-03-25 16:13:27 +0100

    edit path and format date with Carbon class

--- BACK/app/Http/Resources/UserRessource.php
+++ BACK/app/Http/Resources/UserRessource.php
@@ -2,28 +2,29 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserRessource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
-    {
-        return [
-            "id" => $this->id,
-            "name" => $this->name,
-            "first_name" => $this->first_name,
-            "public_name" => $this->public_name,
-            "birth_date" => $this->birth_date,
-            "email" => $this->email,
-            "subscribe_date" => $this->subscribe_date,
-            "image_profile" => $this->img_profile,
-            "campaigns" => CampaignRessource::collection($this->whenLoaded('campaign')),
-        ];
-    }
+  /**
+   * Transform the resource into an array.
+   *
+   * @return array<string, mixed>
+   */
+  public function toArray(Request $request): array
+  {
+    return [
+      "id" => $this->id,
+      "name" => $this->name,
+      "first_name" => $this->first_name,
+      "public_name" => $this->public_name,
+      "birth_date" => Carbon::parse($this->birth_date)->toIso8601String(),
+      "email" => $this->email,
+      "subscribe_date" => Carbon::parse($this->subscribe_date)->toIso8601String(),
+      "image_profile" => $this->img_profile,
+      "campaigns" => CampaignRessource::collection($this->whenLoaded('campaign')),
+    ];
+  }
 }
