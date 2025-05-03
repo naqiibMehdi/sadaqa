62bd6be49d413839cf3bd04d9182b9a482532060
mehdi <mistahbe213@gmail.com>
2025-05-03 18:25:54 +0200

    edit: dockerfile for nginx and edit environment variable

--- FRONT/src/utils/axios.ts
+++ FRONT/src/utils/axios.ts
@@ -3,66 +3,66 @@ import {useAuthStore} from "@/stores/useAuthStore.ts";
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-    }
+  baseURL: import.meta.env.VITE_API_URL + '/api',
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  }
 })
 
 export const fetchData = async (endpoint: string) => {
-    try {
-        const response = await api.get(endpoint)
-        return response.data
-    } catch (error) {
-        if (error instanceof AxiosError) {
-            throw error
-        }
+  try {
+    const response = await api.get(endpoint)
+    return response.data
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw error
     }
+  }
 }
 export const postData = async (endpoint: string, data: object) => {
-    try {
-        const response = await api.post(endpoint, data)
-        return response.data
-    } catch (error) {
-        if (error instanceof AxiosError) {
-            throw error
-        }
+  try {
+    const response = await api.post(endpoint, data)
+    return response.data
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw error
     }
+  }
 }
 
 export const postMultiPartData = async (endpoint: string, data: object) => {
-    try {
-        const response = await api.post(endpoint, data, {headers: {"Content-Type": "multipart/form-data"}})
-        return response.data
-    } catch (error) {
-        if (error instanceof AxiosError) {
-            throw error
-        }
+  try {
+    const response = await api.post(endpoint, data, {headers: {"Content-Type": "multipart/form-data"}})
+    return response.data
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw error
     }
+  }
 }
 
 export const deleteData = async (endpoint: string, data: object) => {
-    try {
-        const response = await api.delete(endpoint, data)
-        return response.data
-    } catch (error) {
-        if (error instanceof AxiosError) {
-            throw error
-        }
+  try {
+    const response = await api.delete(endpoint, data)
+    return response.data
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw error
     }
+  }
 }
 
 api.interceptors.request.use(
     (config) => {
-        const authStore = useAuthStore();
-        const token = authStore.token
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        return config
+      const authStore = useAuthStore();
+      const token = authStore.token
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+      }
+      return config
     },
     (error) => {
-        return Promise.reject(error)
+      return Promise.reject(error)
     }
 )
