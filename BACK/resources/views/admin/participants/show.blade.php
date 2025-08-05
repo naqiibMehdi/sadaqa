5c87c247a5a25dd1837b1170663cc213ad1c5173
mehdi <mistahbe213@gmail.com>
2025-08-05 17:21:13 +0200

    edit: url helper

--- BACK/resources/views/admin/participants/show.blade.php
+++ BACK/resources/views/admin/participants/show.blade.php
@@ -14,7 +14,7 @@
         {{--           class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">--}}
         {{--          <i class="fas fa-edit mr-2"></i>Modifier--}}
         {{--        </a>--}}
-        <a href="{{ route('admin.participants.index') }}"
+        <a href="{{ \App\Helpers\UrlHelper::assetUrl("admin/participants") }}"
            class="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-lg">
           <i class="fas fa-arrow-left mr-2"></i>Retour
         </a>
