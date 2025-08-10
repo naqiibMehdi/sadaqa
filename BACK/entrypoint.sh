74ea35a38f72fe6289f7c3b01ebf273daaaf2118
mehdi <mistahbe213@gmail.com>
2025-08-10 11:31:41 +0200

    edit: dockerfile to add cron package and entrypoint.sh to add php artisan command

--- BACK/entrypoint.sh
+++ BACK/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+service cron start
+
 php artisan migrate:refresh --seed --force
 php artisan config:clear
 php artisan cache:clear
 php artisan storage:link
+php artisan key:generate
 php artisan scribe:generate
 php artisan queue:listen
