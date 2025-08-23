a0ab13c32557b45527fd1396f48cf28c813f1bd6
mehdi <mistahbe213@gmail.com>
2025-08-23 13:20:03 +0200

    edit: redirection

--- BACK/app/Http/Middleware/AdminAuth.php
+++ BACK/app/Http/Middleware/AdminAuth.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Helpers\UrlHelper;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,7 +12,7 @@ class AdminAuth
   public function handle(Request $request, Closure $next)
   {
     if (!Auth::guard('admin')->check()) {
-      return redirect()->route('admin.login')
+      return redirect(UrlHelper::assetUrl('admin/login'))
         ->with('error', 'Vous devez être connecté en tant qu\'administrateur pour accéder à cette page.');
     }
 
